679e8bede3a69cffad321304563bf8f1c47d34fe
Valeriia Buhaiova <bugajovalera@gmail.com>
2024-11-24 11:21:39 +0100

    clean PdfToTxt class and add simple main

--- src/backend/pdf_to_text.py
+++ src/backend/pdf_to_text.py
@@ -5,22 +5,13 @@ import fitz
 
 class PDF_To_Class:
 
-    def __init__(self, filepath_key: str, filepath_input_templates: str, filepath_input: str,):
-        self.filepath_key = filepath_key
-        self.filepath_input_templates = filepath_input_templates
-        self.filepath_input = filepath_input
-        self.api_key= self.reading_key()
-
-    # read API key
-    def reading_key(self) -> str:
-        with open(self.filepath_key, "r") as file:
-            api_key = file.read().strip()
-        return api_key
+    def __init__(self):
+        pass
 
     # reading a pdf file and translating it into text
-    def pdf_to_text_with_format(self) -> str:
+    def pdf_to_text_with_format(self, input_pdf: str) -> str:
         try:
-            with pdfplumber.open(self.filepath_input_templates) as pdf:
+            with pdfplumber.open(input_pdf) as pdf:
                 text = ""
                 for page in pdf.pages:
                     text += page.extract_text() + "\n"
@@ -29,9 +20,9 @@ class PDF_To_Class:
         return text
 
     # signature verification
-    def check_visual_signature(self) -> bool:
+    def check_visual_signature(self, input_pdf: str) -> (bool or None):
         try:
-            doc = fitz.open(self.filepath_input_templates)
+            doc = fitz.open(input_pdf)
             page = doc[len(doc) - 1]
             images = page.get_images(full=True)
             if images:
@@ -39,16 +30,3 @@ class PDF_To_Class:
             return False
         except FileNotFoundError as e:
             return None
-
-    # connect chat gpt
-    def chat_question(self,qustion: str) -> str:
-        MODEL = "gpt-4o"
-        client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", self.api_key))
-        completion = client.chat.completions.create(
-            model=MODEL,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant. Help me with my math homework!"},
-                {"role": "user", "content": qustion}
-            ]
-        )
-        return completion.choices[0].message.content
--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+from src.backend.agent import Agent
+from src.backend.pdf_to_text import PDF_To_Class
+
+load_dotenv('../local.env')
+
 if __name__ == '__main__':
-    print('Hello World!')
+    api_key = os.getenv("OPENAI_API_KEY")
+    model = "gpt-4o"
+    example_pdf = "backend/data/input/example_form.pdf"
+    input_pdf = "backend/data/input/input.pdf"
+
+    pdf_to_txt = PDF_To_Class()
+    example_txt = pdf_to_txt.pdf_to_text_with_format(example_pdf)
+    input_txt = pdf_to_txt.pdf_to_text_with_format(input_pdf)
+    agent = Agent(api_key=api_key, model=model, example_form=example_txt, user_form=input_txt)
+    result = agent.analyze()
+    print(result)
